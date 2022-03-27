8cddba7985e28a135cb46db8e6dc13b8729b0ffd
SidraQurban <sidraqurban77@gmail.com>
2022-03-27 19:44:15 +0500

    done

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+  let aboutStyle = {
+    minHeight: "70vh",
+    margin:"50px "
+  }
+  return (
+    <div  style={aboutStyle}> 
+          This is an about component. 
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae id nihil reiciendis natus, consequuntur doloremque odio labore fugit illum aut! Veritatis quod dolorem repellat itaque blanditiis consectetur neque facere nam tempora, eligendi amet libero, dolores quasi ratione officia provident. Dolore, tempore! Quam quia sint, modi doloremque consequuntur natus qui aliquid accusamus, quaerat libero quasi? Accusantium. </p>
+
+    </div>
+  )
+}
